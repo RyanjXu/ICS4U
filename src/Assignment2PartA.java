101a307fdfd04dbd016eaa24837bf0ad4f7dbd45
RyanjXu <117485040+RyanjXu@users.noreply.github.com>
2025-03-06 10:29:15 -0500

    Finish Assignment2PartA

--- src/Assignment2PartA.java
+++ src/Assignment2PartA.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class Assignment2PartA {
 
     public static int[][] dp;
-    public static String[][] path;
+    public static String[][] path; // store which tile you came from before for backtracking
     public static int ROWS, COLS;
     public static void search(int r, int c, int[][] cost) {
         if(r<0 || c >=COLS) {
@@ -24,10 +24,23 @@ public class Assignment2PartA {
         search(r-1, c, cost);
         search(r, c+1, cost);
     }
+    public static void reconstructPath(int r, int c, int[][] cost, StringBuilder costs, StringBuilder sPath) { // helper method to backtrack the path
+        costs.insert(0, cost[r][c]+" ");
+        if(r==ROWS-1 && c==0) {
+            return;
+        }
+        sPath.insert(0, path[r][c]+' ');
+        if(path[r][c].equals("NORTH")) {
+            reconstructPath(r+1, c, cost, costs, sPath);
+        } else {
+            reconstructPath(r, c-1, cost, costs, sPath);
+        }
+    }
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int T = s.nextInt();
-        while(T-->0) {
+        System.out.println("Finding the Cheapest Routes: ");
+        for(int gNum =1; gNum<=T; gNum++) {
             ROWS=  s.nextInt(); COLS = s.nextInt();
             int[][] cost = new int[ROWS][COLS];
             dp = new int[ROWS][COLS]; // standard dp to track min cost to get ot each indice
@@ -44,8 +57,20 @@ public class Assignment2PartA {
             path[ROWS-2][0]= "NORTH";
             path[ROWS-1][1]= "EAST";
 
+            StringBuilder costs = new StringBuilder(), sPath= new StringBuilder();
+            reconstructPath(0, COLS-1, cost, costs, sPath);
             int cheapestCost = dp[0][COLS-1];
-            System.out.println(cheapestCost);
+
+            System.out.printf("\nGrid #%d: \n", gNum);
+            for(int[] row : cost) {
+                for(int col : row) {
+                    System.out.printf("%d ", col);
+                }
+                System.out.println();
+            }
+            System.out.printf("Cheapest Route: %s\n", costs);
+            System.out.printf("Direction: %s\n", sPath);
+            System.out.printf("Cheapest Cost: $%d\n", cheapestCost);
         }
     }
 }
