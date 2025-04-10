68a2cadb6bf18454eb462ecd679ba90815152fb8
RyanjXu <ryanjiahuaxu@gmail.com>
2025-04-09 21:56:29 -0400

    Update Driver.java

--- src/assignment4/Driver.java
+++ src/assignment4/Driver.java
@@ -1,10 +1,13 @@
 package assignment4;
 import java.io.*;
+import java.util.*;
 
 public class Driver {
+    ArrayList<TVShow> shows = new ArrayList<>();
     public static int displayMenu (int menuNum, BufferedReader stdIn) throws IOException {
-
+        int maxchoice;
         if (menuNum == 0) {
+            maxchoice = 3;
             System.out.println ("----------  MAIN MENU  -----------");
             System.out.println ("1) Accessing your TV shows list");
             System.out.println ("2) Accessing within a particular TV show");
@@ -12,6 +15,7 @@ public class Driver {
             System.out.println ("----------------------------------");
         }
         else if (menuNum == 1) {
+            maxchoice = 6;
             System.out.println ("\n---------  SUB-MENU #1  ----------");
             System.out.println ("1) Display a list of all your TV shows"); // Just the show titles, numbered in order
             System.out.println ("2) Display info on a particular TV show");
@@ -22,6 +26,7 @@ public class Driver {
             System.out.println ("----------------------------------");
         }
         else {
+            maxchoice = 7;
             System.out.println ("\n---------  SUB-MENU #2  ----------");
             System.out.println ("1) Display all episodes (in the last sorted order) ");
             System.out.println ("2) Display info on a particular episode ");
@@ -33,20 +38,47 @@ public class Driver {
             System.out.println ("----------------------------------");
         }
 
-        System.out.print ("\n\nPlease enter your choice:  ");
-        int choice = Integer.parseInt (stdIn.readLine ());
 
+        int choice = 0;
+        while(choice<1 || choice>maxchoice) {
+            System.out.print ("\nPlease enter your choice:  ");
+            try {
+                choice = Integer.parseInt(stdIn.readLine());
+            } catch (NumberFormatException e) {
+                System.out.println ("Invalid Choice!");
+            }
+        }
         return choice;
     }
-
-
     public static void main (String[] args) throws IOException {
         BufferedReader stdIn = new BufferedReader (new InputStreamReader (System.in));
         int mainMenuChoice, subMenuChoice;
         mainMenuChoice = displayMenu (0, stdIn);
 
-        if (mainMenuChoice == 1)
+        if (mainMenuChoice == 1) {
             subMenuChoice = displayMenu (1, stdIn);
+            switch(subMenuChoice) {
+                case 1:
+
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+
+                    break;
+                case 4:
+
+                    break;
+                case 5:
+
+                    break;
+                case 6:
+
+                    break;
+            }
+        }
+
         else if (mainMenuChoice == 2)
             subMenuChoice = displayMenu (2, stdIn);
     }
